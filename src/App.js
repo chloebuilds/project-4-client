cee2877e7612cfef80961ec172446fdffa61b567
Chloe Brown <chloemail@protonmail.com>
2021-06-15 23:07:19 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 function App() {
-  React.useEffect(() => {
-    const getData = async () => {
-      const res = await fetch('/api/endpoint') // * <-- replace with your endpoint
-      const data = await res.json()
-      console.log(data)
-    }
-    getData()
-  })
 
-  return <h1>Hello World</h1>
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h3>I am Zenith</h3>
+    </>
+  )
 }
 
 export default App
