5881e7f6997ce37f5f9a21d71225975591f820af
alara <alaraayan@hotmail.com>
2021-06-19 00:09:55 +0100

    404-401-pages

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 
         
           {/* for any page not found */}
-          <Route path="/unauthorized" component={NotAuthorized} />
+          <Route path="/401" component={NotAuthorized} />
           <Route exact path="*" component={NotFound} />
         
         </Switch>
--- src/components/zenith/SprintGoals.js
+++ src/components/zenith/SprintGoals.js
@@ -1,35 +1,79 @@
 import React from 'react'
-
 import { UserContext } from '../context/UserContext'
 
-function SprintGoal() {
-
+export default function SprintGoalsDisplay() {
   const { currentSprint } = React.useContext(UserContext)
-
-  
+  const [ goals, setGoals ] = React.useState([])
   const isLoading = !currentSprint
+  const currentGoals = currentSprint?.sprintGoals
 
+  React.useEffect(() => {
+    const getCurrentGoals = () => {
+      setGoals(currentGoals)
+    }
+    getCurrentGoals()
+  }, [currentGoals])
 
   return (
     <>
-      {isLoading && <div><p>loading...</p></div>}
-      {currentSprint &&
-      <> 
-        <h3>Sprint Goals</h3>
-        {currentSprint?.sprintGoals.map(goal => (
+      {isLoading && <div><p>ॐ..loading...ॐ</p></div>}
+      {currentSprint && 
+      <>
+        <h3>goals</h3>
+        {goals?.map(goal => (
           <div key={goal.id}>
-            <p>{goal.goalName}</p>
-            <p>{goal.goalDescription}</p>
-
+            <ul>
+              <li>
+                <h4>{goal.goalName}</h4>
+                <p>{goal.goalDescription}</p>
+              </li>
+            </ul>
           </div>
-          
         ))}
       </>
       }
-    
     </>
   )
-
 }
 
-export default SprintGoal
+
+
+
+
+
+
+// import React from 'react'
+
+// import { UserContext } from '../context/UserContext'
+
+// function SprintGoal() {
+
+//   const { currentSprint } = React.useContext(UserContext)
+
+  
+//   const isLoading = !currentSprint
+
+
+//   return (
+//     <>
+//       {isLoading && <div><p>loading...</p></div>}
+//       {currentSprint &&
+//       <> 
+//         <h3>Sprint Goals</h3>
+//         {currentSprint?.sprintGoals.map(goal => (
+//           <div key={goal.id}>
+//             <p>{goal.goalName}</p>
+//             <p>{goal.goalDescription}</p>
+
+//           </div>
+          
+//         ))}
+//       </>
+//       }
+    
+//     </>
+//   )
+
+// }
+
+// export default SprintGoal
